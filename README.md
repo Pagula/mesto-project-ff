1e400321c536b2e8a987ebc7d20d20c6a5ea96a9
Juriy Pagula <yurypagula@yandex.ru>
2025-01-04 10:47:09 +0300

    Первая итерация

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Pagula/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
-
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
 // @todo: Функция создания карточки
+function createCard(card, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const image = cardElement.querySelector('.card__image');
+    const title = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
 
+    image.src = card.link; 
+    image.alt = card.name; 
+    title.textContent = card.name;
+
+    deleteButton.addEventListener('click', () => {
+        deleteCard(cardElement);
+    });
+
+    return cardElement;
+}
 // @todo: Функция удаления карточки
+function removeCard(cardElement) {
+    cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(card => {
+    const cards = createCard(card, removeCard);
+    placesList.append(cards);
+});
